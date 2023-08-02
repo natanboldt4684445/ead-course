178851612ab680433af769f26d11c83b1d243854
Natan <natanboldt@gmail.com>
2023-08-01 21:35:20 -0300

    Advanced filters

--- src/main/java/com/ead/course/models/LessonModel.java
+++ src/main/java/com/ead/course/models/LessonModel.java
@@ -30,7 +30,7 @@ public class LessonModel implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private ModuleModel module;
 
